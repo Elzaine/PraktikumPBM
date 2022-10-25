bf3eb82245632a9ec2bc7d75ef58c60384007d24
Elzaine <mirzaalghivary28@gmail.com>
2022-10-25 18:41:16 +0700

    first commit

---
+++ Tugas1/main1.dart
@@ -0,0 +1,32 @@
+void main() {
+  int Integer = 28;
+
+  num Numb = 7.77;
+
+  double Double = 0.69;
+
+  String Strings = "Mirza Muhamad Alghivary";
+
+  bool Booleans = false;
+
+  const word = 'HATE';
+  final Runess = word.runes.toList();
+
+  dynamic Dynamic = 160;
+  Dynamic = "Seratus";
+
+  Symbol Symbols = new Symbol("Ini adalah simbol");
+
+  String? Nulls; //
+
+  print(Integer);
+  print(Numb);
+  print(Double);
+  print(Strings);
+  print(Booleans);
+  print(Runess);
+  print(Dynamic);
+  print(Symbols);
+  print(#IniSimbol);
+  print("$Nulls");
+}
